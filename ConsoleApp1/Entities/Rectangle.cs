09ec0dfd0bb1973069e0aa7fefae9649713dbfa0
Jonathan Ventura <jonathanlventura3@gmail.com>
2022-05-12 19:54:36 -0300

    Primeiro commit

---
+++ ConsoleApp1/Entities/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Entities
+{
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(Color color, double width, double height) : base(color)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
+    }
+}
